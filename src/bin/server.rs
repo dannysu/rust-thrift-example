92ef6cc12f5f422c11e5ef061f202b81cfd55517
Danny Su <contact@dannysu.com>
2019-05-05 15:11:09 -0700

    Don't need to use Box

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,8 +1,6 @@
-use thrift::protocol::{TCompactInputProtocolFactory, TInputProtocolFactory};
-use thrift::protocol::{TCompactOutputProtocolFactory, TOutputProtocolFactory};
+use thrift::protocol::{TCompactInputProtocolFactory, TCompactOutputProtocolFactory};
 use thrift::server::TServer;
-use thrift::transport::{TBufferedReadTransportFactory, TReadTransportFactory};
-use thrift::transport::{TBufferedWriteTransportFactory, TWriteTransportFactory};
+use thrift::transport::{TBufferedReadTransportFactory, TBufferedWriteTransportFactory};
 
 use rust_thrift_example::{ExampleServiceSyncHandler, ExampleServiceSyncProcessor};
 
@@ -21,12 +19,12 @@ fn run() -> thrift::Result<()> {
     let listen_address = "127.0.0.1:9000";
 
     // create input protocol/transport factory
-    let i_tran_fact: Box<TReadTransportFactory> = Box::new(TBufferedReadTransportFactory::new());
-    let i_prot_fact: Box<TInputProtocolFactory> = Box::new(TCompactInputProtocolFactory::new());
+    let i_tran_fact = TBufferedReadTransportFactory::new();
+    let i_prot_fact = TCompactInputProtocolFactory::new();
 
     // create output protocol/transport factory
-    let o_tran_fact: Box<TWriteTransportFactory> = Box::new(TBufferedWriteTransportFactory::new());
-    let o_prot_fact: Box<TOutputProtocolFactory> = Box::new(TCompactOutputProtocolFactory::new());
+    let o_tran_fact = TBufferedWriteTransportFactory::new();
+    let o_prot_fact = TCompactOutputProtocolFactory::new();
 
     // create the server and start listening
     let processor = ExampleServiceSyncProcessor::new(ExampleServiceHandlerImpl {});
